922fb91d56f3315b39f0ff16a3ed862ada40356d
dhani99 <dhwani1999thakar@gmail.com>
2021-11-30 12:19:51 +1300

    pushing changes

---
+++ MagicYearCalculatorApp/MagicYearCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Magic_Year_Calculator_App
+{
+    public  class MagicYearCalculator
+    {
+        
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public int CalculateMonthlySalary(int annualSalary)
+        {
+            var monthlySalary = annualSalary / 12;
+            return monthlySalary; 
+        }
+
+        public int CalculateMagicYear(int startYear)
+        {
+            var magicYear = startYear + 65;
+            return magicYear;
+        }
+    
+    }
+}
---
+++ MagicYearCalculatorConsole/UserInput.cs
@@ -0,0 +1,7 @@
+namespace MagicYearCalculatorConsole
+{
+    public class UserInput
+    {
+        
+    }
+}
